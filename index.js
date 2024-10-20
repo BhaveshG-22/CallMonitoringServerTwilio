8fc7742e3f646d4eb4bfe7d6c262cde7dcaa9d98
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-10-20 05:36:06 -0400

    added routes to receive and process data and added mockData json file

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to our server dashboard!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.query);
+  res.res(200).send("Successfully Data RECEIVED");
 });
 
 app.listen(port, () => {
