4567447152d2165a30761f67845eb696214b196b
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-10-20 05:46:10 -0400

    added vercel.json file and changed index file to support vercel

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to our server dashboard!");
@@ -9,9 +7,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log(req.query);
-  res.res(200).send("Successfully Data RECEIVED");
+  res.status(200).send("Successfully Data RECEIVED");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
